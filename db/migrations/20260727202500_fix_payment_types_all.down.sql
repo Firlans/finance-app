b4b6bd2077d81bef12b71cb962a7c124b2da3388
Firlans <firlansyah54321@gmail.com>
2026-07-27 20:40:18 +0700

    Update down migrations to actively revert data

--- db/migrations/20260727202500_fix_payment_types_all.down.sql
+++ db/migrations/20260727202500_fix_payment_types_all.down.sql
@@ -1,2 +1,8 @@
--- Down migration does nothing because we are just correcting data.
--- If needed, revert logic to whatever it was before.
+-- Revert payment types back to the state of the very first migration
+-- (debit -> increase, credit -> decrease)
+UPDATE payments
+SET type = CASE 
+    WHEN (SELECT transaction_type FROM transactions WHERE transactions.id = payments.transaction_id) = 'debit' THEN 'increase'
+    ELSE 'decrease'
+END
+WHERE transaction_id IS NOT NULL;
--- db/migrations/20260727203600_fix_debt_payment_types_live.down.sql
+++ db/migrations/20260727203600_fix_debt_payment_types_live.down.sql
@@ -1,1 +1,9 @@
--- Down migration does nothing because we are just correcting data on live.
+-- Revert payment types for Debt back to the state of the very first migration
+-- (debit -> increase, credit -> decrease)
+UPDATE payments
+SET type = CASE 
+    WHEN (SELECT transaction_type FROM transactions WHERE transactions.id = payments.transaction_id) = 'debit' THEN 'increase'
+    ELSE 'decrease'
+END
+WHERE transaction_id IS NOT NULL
+  AND loan_id IN (SELECT id FROM loans WHERE loan_type = 'debt');
