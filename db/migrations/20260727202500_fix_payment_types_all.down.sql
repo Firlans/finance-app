1ba9be759ebe52cf416343c2f5ba21286aec5d3b
Firlans <firlansyah54321@gmail.com>
2026-07-27 20:47:06 +0700

    Remove the 2 incorrect fix migration files

--- db/migrations/20260727202500_fix_payment_types_all.down.sql
+++
@@ -1,8 +0,0 @@
--- Revert payment types back to the state of the very first migration
--- (debit -> increase, credit -> decrease)
-UPDATE payments
-SET type = CASE 
-    WHEN (SELECT transaction_type FROM transactions WHERE transactions.id = payments.transaction_id) = 'debit' THEN 'increase'
-    ELSE 'decrease'
-END
-WHERE transaction_id IS NOT NULL;
--- db/migrations/20260727203600_fix_debt_payment_types_live.down.sql
+++
@@ -1,9 +0,0 @@
--- Revert payment types for Debt back to the state of the very first migration
--- (debit -> increase, credit -> decrease)
-UPDATE payments
-SET type = CASE 
-    WHEN (SELECT transaction_type FROM transactions WHERE transactions.id = payments.transaction_id) = 'debit' THEN 'increase'
-    ELSE 'decrease'
-END
-WHERE transaction_id IS NOT NULL
-  AND loan_id IN (SELECT id FROM loans WHERE loan_type = 'debt');
