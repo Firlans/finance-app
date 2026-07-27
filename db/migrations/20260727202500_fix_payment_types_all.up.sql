ee92b4687fe30b40a6e49ffa571179950a18cd2f
Firlans <firlansyah54321@gmail.com>
2026-07-27 20:32:44 +0700

    Remove receivable logic from fix migration per user request

--- db/migrations/20260727202500_fix_payment_types_all.up.sql
+++ db/migrations/20260727202500_fix_payment_types_all.up.sql
@@ -12,14 +12,4 @@ END
 WHERE transaction_id IS NOT NULL
   AND loan_id IN (SELECT id FROM loans WHERE loan_type = 'debt');
 
--- For Receivable (Piutang):
--- Kas Keluar (credit) = Penambahan Piutang (increase)
--- Kas Masuk (debit) = Pembayaran Piutang (decrease)
-UPDATE payments
-SET type = CASE 
-    WHEN (SELECT transaction_type FROM transactions WHERE transactions.id = payments.transaction_id) = 'credit' THEN 'increase'
-    WHEN (SELECT transaction_type FROM transactions WHERE transactions.id = payments.transaction_id) = 'debit' THEN 'decrease'
-    ELSE type
-END
-WHERE transaction_id IS NOT NULL
-  AND loan_id IN (SELECT id FROM loans WHERE loan_type = 'receivable');
+-- Note: Receivable (Piutang) is NOT modified here because the user confirmed the previous migration logic for receivable was already correct.
